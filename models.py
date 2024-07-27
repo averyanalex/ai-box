1a97d8d7ca087e32c4ee7c8ec8a46509d3824281
AveryanAlex <alex@averyan.ru>
2024-07-27 14:25:36 +0300

    Use SigLIP

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from transformers import CLIPProcessor, CLIPModel, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoProcessor, AutoModel, AutoTokenizer, AutoModelForSeq2SeqLM
 
-clip_name = "laion/CLIP-ViT-L-14-DataComp.XL-s13B-b90K"
-clip_model = CLIPModel.from_pretrained(clip_name)
-clip_processor = CLIPProcessor.from_pretrained(clip_name)
+clip_name = "google/siglip-so400m-patch14-384"
+clip_model = AutoModel.from_pretrained(clip_name)
+clip_processor = AutoProcessor.from_pretrained(clip_name)
 
 translation_name = "Helsinki-NLP/opus-mt-ru-en"
 translation_tokenizer = AutoTokenizer.from_pretrained(translation_name)
